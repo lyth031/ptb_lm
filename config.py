a37233d9ba09a192ee573a0578e4251028e78415
lyth031 <lyth031@outlook.com>
2018-05-31 15:38:55 +0800

    test

--- config.py
+++ config.py
@@ -1,47 +1,21 @@
 # -*- coding: utf-8 -*-
 class Config(object):
-	'''
-	lstm config:
-	Speed: 61566 wps
-	Train Perplexity: 43.167
-	Valid Perplexity: 116.618
-	Test Perplexity: 111.888
-	'''
-    # init_scale = 0.1
-    # learning_rate = 1.0
-    # max_grad_norm = 5
-    # num_layers = 2
-    # slice_size = 20
-    # hidden_size = 200
-    # max_epoch = 13
-    # keep_prob = 1.0
-    # lr_const_epoch = 4
-    # lr_decay = 0.5
-    # batch_size = 20
-    # vocab_size = 10000
-    # rnn_model = "lstm"
-    # data_path = "../simple-examples/data/"
-    # save_path = "../out/cudnn/lstm/"
 
-	'''
-	gru config:
-	Speed: 76442 wps
-	Train Perplexity: 51.346
-	Valid Perplexity: 111.628
-	Test Perplexity: 106.113
-	'''
-    init_scale = 0.1
-    learning_rate = 1.0
-    max_grad_norm = 5
-    num_layers = 2
-    slice_size = 30
-    hidden_size = 200
-    max_epoch = 13
-    keep_prob = 0.8
-    lr_const_epoch = 4
-    lr_decay = 0.7
-    batch_size = 30
-    vocab_size = 10000
-    rnn_model = "gru"
-    data_path = "./data/"
-    save_path = "../out/cudnn/gru/"
+    def __init__(self):
+        self.init_scale = 0.1
+        self.learning_rate = 1.0
+        self.max_grad_norm = 5
+        self.num_layers = 2
+        self.slice_size = 30
+        self.hidden_size = 200
+        self.max_epoch = 13
+        self.keep_prob = 0.8
+        self.lr_const_epoch = 4
+        self.lr_decay = 0.7
+        self.batch_size = 30
+        self.vocab_size = 10000
+        self.rnn_model = "gru"
+        self.data_path = "./data/"
+        self.save_path = "../out/cudnn/gru/"
+
+
